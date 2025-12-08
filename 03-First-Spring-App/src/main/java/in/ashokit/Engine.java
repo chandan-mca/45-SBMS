3f8c74e2851dbd35a08a026289181bc9181fa510
Chandan Agrawal <cagrawal0012@gmail.com>
2025-12-08 22:59:37 +0530

    First Spring App with XML config

---
+++ 03-First-Spring-App/src/main/java/in/ashokit/Engine.java
@@ -0,0 +1,14 @@
+package in.ashokit;
+
+public class Engine {
+
+	public Engine() {
+		System.out.println("Engine - Constructor Executed...");
+	}
+	
+	 public int start() {
+		 System.out.println("Engine started...");
+		 return 1;
+	 }
+
+}
