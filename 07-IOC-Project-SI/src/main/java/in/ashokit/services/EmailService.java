3624a0765f720109a4852f15e6e961ad56afcf4e
Chandan Agrawal <cagrawal0012@gmail.com>
2025-12-10 21:36:14 +0530

    Project on DI

---
+++ 07-IOC-Project-SI/src/main/java/in/ashokit/services/EmailService.java
@@ -0,0 +1,13 @@
+package in.ashokit.services;
+
+public class EmailService {
+
+	public EmailService() {
+		System.out.println("EmailService - Constructor...");
+	}
+
+	public void emailSent(String email) {
+		System.out.println("Email Sent Successfully...");
+	}
+
+}
---
+++ 07-IOC-Project-SI/src/main/java/in/ashokit/services/PasswordService.java
@@ -0,0 +1,13 @@
+package in.ashokit.services;
+
+public class PasswordService {
+
+	public PasswordService() {
+		System.out.println("PasswordService - Constructor...");
+	}
+
+	public void passwordEncrypt(String password) {
+		System.out.println("Password Encrypted Successfully...");
+	}
+
+}
