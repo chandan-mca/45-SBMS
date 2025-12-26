173ec112a868bc6118ae761c1731b1e5bd0501c8
Chandan Agrawal <cagrawal0012@gmail.com>
2025-12-26 21:53:46 +0530

    Runners

---
+++ 16-Runners-App/src/main/java/in/ashokit/beans/MyCmdRunner.java
@@ -0,0 +1,15 @@
+package in.ashokit.beans;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyCmdRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("MyCmdRunner :: run() called...");
+
+	}
+
+}
